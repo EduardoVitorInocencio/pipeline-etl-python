4f5003399d58a38085be92e8cfa30b453c017df1
EduardoVitorInocencio <eduardovitor16@gmail.com>
2024-10-03 22:17:38 -0300

    Configurando o dbt core

---
+++ src/extract_load.py
@@ -0,0 +1,45 @@
+import yfinance as yf
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Obter as variáveis do arquivo .env
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+# Criar a URL de conexão do banco de dados
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+# Lista de commodities
+commodities = ['CL=F', 'GC=F', 'SI=F']  # Petróleo bruto, Ouro, Prata
+
+def buscar_dados_commodities(simbolo, periodo='5d', intervalo='1d'):
+    ticker = yf.Ticker(simbolo)
+    dados = ticker.history(period=periodo, interval=intervalo)[['Close']]
+    # dados['simbolo'] = simbolo  # Adiciona a coluna do símbolo
+    return dados
+
+def buscar_todos_dados_commodities():
+    todos_dados = []
+    for simbolo in commodities:
+        dados = buscar_dados_commodities(simbolo)
+        todos_dados.append(dados)
+    return pd.concat(todos_dados)  # Concatena todos os dados em um único DataFrame
+
+def salvar_no_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+    print(f"Dados salvos no schema '{schema}' do banco de dados PostgreSQL")
+
+if __name__ == "__main__":
+    dados_concatenados = buscar_todos_dados_commodities()
+    salvar_no_postgres(dados_concatenados, schema='public')
+    # print(DATABASE_URL)
